d14b4ac49ce40bcc1acc95816f4431e7f38759fb
Aljaz Hericko <aljaz-h@hotmail.com>
2024-02-17 19:32:13 +0100

    [A-002] Insert code into table

--- src/main/java/org/paurus/ImportApplication.java
+++ src/main/java/org/paurus/ImportApplication.java
@@ -4,6 +4,9 @@ import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
 import java.sql.*;
 
 public class ImportApplication {
@@ -12,7 +15,12 @@ public class ImportApplication {
         String url = "jdbc:h2:~/test";
         try (Connection conn = DriverManager.getConnection(url, "sa", ""); Statement statement = conn.createStatement()) {
             initializeDB(statement);
-            insertDataInDB(statement);
+
+            String insertQuery = "INSERT INTO DATA (match_id, market_id, outcome_id, specifiers)" + "VALUES (?, ?, ?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(insertQuery)) {
+                insertDataInDB(preparedStatement);
+            }
+
             queryDB(statement);
         }
     }
@@ -24,10 +32,10 @@ public class ImportApplication {
                 DROP TABLE IF EXISTS DATA;
                 CREATE TABLE DATA (
                     id INT AUTO_INCREMENT PRIMARY KEY,
-                    match_id INT NOT NULL,
+                    match_id VARCHAR(100) NOT NULL,
                     market_id INT NOT NULL,
                     outcome_id VARCHAR(50) NOT NULL,
-                    specifiers VARCHAR(1000) NOT NULL,
+                    specifiers VARCHAR(1000),
                     date_insert TIMESTAMP DEFAULT CURRENT_TIMESTAMP
                 );
                 """;
@@ -37,13 +45,25 @@ public class ImportApplication {
     }
 
     @SneakyThrows
-    private static void insertDataInDB(Statement statement) {
+    private static void insertDataInDB(PreparedStatement statement) {
         System.out.println("Starting data insertion");
 
-        String sql = "INSERT INTO DATA (match_id, market_id, outcome_id, specifiers)" + "VALUES (1, 1, '2', 'RANDOM TEXT')";
-        statement.executeUpdate(sql);
-        String sql2 = "INSERT INTO DATA (match_id, market_id, outcome_id, specifiers)" + "VALUES (2, 2, '3', 'RANDOM TEXT2')";
-        statement.executeUpdate(sql2);
+        BufferedReader br = new BufferedReader(new InputStreamReader(ImportApplication.class.getResourceAsStream("/fo_random.txt")));
+        String st;
+        br.readLine();
+        while ((st = br.readLine()) != null) {
+            String[] split = st.split("\\|");
+            statement.setString(1, split[0]);
+            statement.setInt(2, Integer.parseInt(split[1]));
+            statement.setString(3, split[2]);
+            if (split.length > 3) {
+                statement.setString(4, split[3]);
+            } else {
+                statement.setNull(4, java.sql.Types.NULL);
+            }
+            statement.execute();
+            statement.clearParameters();
+        }
 
         System.out.println("Completed data insertion");
     }
@@ -52,7 +72,7 @@ public class ImportApplication {
     private static void queryDB(Statement statement) {
         System.out.println("Starting data evaluation");
 
-        try(ResultSet resultSetMin = statement.executeQuery("SELECT MIN(date_insert) AS min_insert_date FROM DATA")) {
+        try (ResultSet resultSetMin = statement.executeQuery("SELECT MIN(date_insert) AS min_insert_date FROM DATA")) {
             if (resultSetMin.first()) {
                 System.out.println("Minimum insertion date: " + resultSetMin.getTimestamp("min_insert_date"));
             }
